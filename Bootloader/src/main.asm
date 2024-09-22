08ec2e83e73285b192c73295a139077263d26902
Serge Knatsko <sknatsko@gmail.com>
2024-09-22 14:23:41 -0400

    prints hello world

--- Bootloader/src/main.asm
+++ Bootloader/src/main.asm
@@ -13,10 +13,11 @@ puts:
     lodsb
     or al,al
     jz .done
-    
-    mov ah, 0x0e
-    mov bh, 0
-    int 0x10
+
+    mov ah, 0x0e ; Function to print a character
+    mov bh, 0    ; Page number (0 for current page)
+    int 0x10     ; Call BIOS to print character
+
 .done:
     pop ax
     pop si
@@ -32,7 +33,7 @@ main:
     ;setup stack
     mov ss, ax
     mov sp, 0x7C00
-
+    
     mov si,msg_hello
     call puts
 
@@ -40,7 +41,8 @@ main:
 .halt:
     jmp .halt
 
-msg_hello: db 'Hello World',0x0A,0
+msg_hello: db 'Hello World',0x0A,0   
+
 
 times 510-($-$$) db 0
 dw 0AA55h
