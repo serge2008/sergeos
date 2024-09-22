8ad5130599d23e680b8e9ab30594bd6a40aea712
Serge Knatsko <sknatsko@gmail.com>
2024-09-22 14:10:42 -0400

    edited main.asm in bootloader

--- Bootloader/src/main.asm
+++ Bootloader/src/main.asm
@@ -1,8 +1,34 @@
 org 0x7C00
 bits 16
 
+start:
+    jmp main
+
+
+puts:
+    push si
+    push ax
+
+.loop:
+    lodsb
+    or al,al
+    jz .done
+.done:
+    pop ax
+    pop si
+    ret
+
 
 main:
+    ;setup data segments
+    mov ax, 0
+    mov ds, ax
+    mov es ,ax
+    
+    ;setup stack
+    mov ss, ax
+    mov sp, 0x7C00
+
     hlt
 .halt:
     jmp .halt
